a3e571a2a67d4a09b6938680dd95a92361d73243
morten-s <ms@meetpeople.dk>
2023-09-18 16:31:47 +0200

    gitignore

--- SpilleKort/SpilleKort.cs
+++ SpilleKort/SpilleKort.cs
@@ -12,8 +12,10 @@ namespace SpilleKort
 
         public Kort HentKort()
         {
-            int kort = new Random().Next(55); // 0 til 51 + 3 joker
-            if(kort >= 52)return new Kort(kortFarve[4] + " " + kort);
+            int kort = new Random().Next(55); // 52kort + 3 joker, værdier mellem 0 - 54
+            //Er det en jooker?
+            if(kort >= 52 && kort < 55)return new Kort(kortFarve[4] + " " + kort);
+            //Er det et alm kort
             return new Kort(kortFarve[(kort/13)] + kortNavn[(kort%13)]+ " " + kort);
         }
     }
